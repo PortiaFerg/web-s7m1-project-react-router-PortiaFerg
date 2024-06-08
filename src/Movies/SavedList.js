c49a22cae73d1ca9d950bb294e6848bc45eeccf9
Portia Ferguson <portiaroyalty@gmail.com>
2024-06-07 22:35:07 -0600

    Update SavedList.js

--- src/Movies/SavedList.js
+++ src/Movies/SavedList.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function SavedList(props) {
+  const navigate = useNavigate()
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
-         <Link key={movie.id} to={`/movies/${movie.id}`} className="saved-movie">
-         {movie.title}
-       </Link>
+        <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">
-        <Link to="/">Home</Link>
-      </div>
+      <div onClick={() => navigate('/')} className="home-button">Home</div>
     </div>
   );
 }
+
 
